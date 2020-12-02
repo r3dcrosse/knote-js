09df17e57e19e15291e09a7ad33aa6367fd51673
r3dcrosse <mrdavidwayman@gmail.com>
2020-12-01 21:44:56 -0800

    Refactors app to be stateless, displays images in notes on all pods

--- 01/index.js
+++ 01/index.js
@@ -1,8 +1,10 @@
 const path = require('path')
 const express = require('express')
+const fs = require('fs')
 const MongoClient = require('mongodb').MongoClient
 const multer = require('multer')
 const marked = require('marked')
+const { ObjectId } = require('mongodb')
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -38,15 +40,27 @@ async function start() {
     res.render('index', { notes: await retrieveNotes(db) })
   })
 
+  app.get('/uploads/:id', async (req, res) => {
+    const id = req.params.id;
+    
+    const img = await getImage(db, id);
+    res.set('Content-Type', img.contentType)
+    res.send(Buffer.from(img.data.buffer));
+  })
+
   app.post(
     '/note',
     multer({ dest: path.join(__dirname, 'public/uploads/') }).single('image'),
     async (req, res) => {
       if (!req.body.upload && req.body.description) {
-        await saveNote(db, { description: req.body.description })
+        await saveNote(db, { type: "note", description: req.body.description })
         res.redirect('/')
       } else if (req.body.upload && req.file) {
-        const link = `/uploads/${encodeURIComponent(req.file.filename)}`
+        
+        // Save the image in mongo
+        const { insertedId } = await saveImage(db, path.join(__dirname, `public/uploads/${req.file.filename}`))
+        const link = `/uploads/${insertedId}`
+    
         res.render('index', {
           content: `${req.body.description} ![](${link})`,
           notes: await retrieveNotes(db),
@@ -64,10 +78,22 @@ async function saveNote(db, note) {
   await db.insertOne(note)
 }
 
+async function saveImage(db, imagePath) {
+    const newImage = { type: "image", data: fs.readFileSync(imagePath), contentType: 'image/png' };
+    return await db.insertOne(newImage)
+}
+
+async function getImage(db, id) {
+    return await db.findOne(ObjectId(id))
+}
+
 async function retrieveNotes(db) {
-  const notes = (await db.find().toArray()).reverse()
+  const notesAndImages = (await db.find().toArray()).reverse()
+
+  const notes = notesAndImages.filter(({ type }) => type === "note");
+
   return notes.map(it => {
-    return { ...it, description: marked(it.description) }
+    return { description: it.description ? marked(it.description) : ""}
   })
 }
 
